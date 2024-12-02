3773d8395051d457e1de0edffa650615df9e0e44
satoren <satoreyo@hotmail.com>
2024-12-02 13:24:27 +0900

    initial version

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    alias: {
-      'vitest-environment-node-with-websockets':
-        './vitest.node-with-websockets',
-    },
   },
 })
